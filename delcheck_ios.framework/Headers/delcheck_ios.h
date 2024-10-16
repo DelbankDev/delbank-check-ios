c36ba477584e4a1b30953be4348e4755f5fa121d
Douglas Abreu <douglasabrel97@gmail.com>
2024-10-16 16:13:55 -0300

    feat: add framework binary

---
+++ delcheck_ios.framework/Headers/delcheck_ios.h
@@ -0,0 +1,18 @@
+//
+//  delcheck_ios.h
+//  delcheck-ios
+//
+//  Created by Diego Henrique de Abreu on 16/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for delcheck_ios.
+FOUNDATION_EXPORT double delcheck_iosVersionNumber;
+
+//! Project version string for delcheck_ios.
+FOUNDATION_EXPORT const unsigned char delcheck_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <delcheck_ios/PublicHeader.h>
+
+
